808c581c70db75b4dfcca31a56c9c6c30845749c
chebilyoussef21 <youssefchebil@vt.edu>
2025-02-21 16:49:22 -0500

    youssef here

--- leg1_ws/build/leg1_description/prefix_override/sitecustomize.py
+++ leg1_ws/build/leg1_description/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/houndsito/vt-cro/robogamescode/leg1_ws/install/leg1_description'
+    sys.prefix = sys.exec_prefix = '/home/vtcro/robogamescode/leg1_ws/install/leg1_description'
