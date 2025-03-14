3b0008cf1c9037fe84de8fc3665c226970a9cbdc
houndsito <fcampoverdeg@vt.edu>
2025-03-14 13:17:33 -0400

    removed camera executable

--- car_ws/camera_gazebo.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-bash & source /opt/ros/jazzy/setup.bash
-source install/setup.bash
-ros2 launch articubot_one launch_sim.launch.py world:=./src/articubot_one/worlds/obstacles.world 
-exit
